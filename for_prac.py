43519297276c9f47bd0b4831d5c4320c95962395
OtakeYuto <yuto.programming@gmail.com>
2023-01-07 16:47:00 +0900

    反復処理を追加

--- for_prac.py
+++ for_prac.py
@@ -1,2 +1,10 @@
 for i in range(10):
   print(i)
+
+l = ['zero','one','two','three','four','five','six','seven','eight','nine','ten',]
+for i in l:
+  print(i)
+
+print(len(l))
+for i in range(len(l)):
+  print(i)
