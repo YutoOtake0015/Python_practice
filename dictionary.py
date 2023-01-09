e1e20ba19a3b155365cb587e64828d6b2074623e
OtakeYuto <yuto.programming@gmail.com>
2023-01-09 20:43:43 +0900

    辞書型の動作確認

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+print('-- START')
+dic1 = {'banana': 200, 'apple': 150, 'orange': 80}
+print(dic1)
+
+dic1['melon'] = 400
+
+for k, v in dic1.items():
+  print(k, v)
+
+for k in dic1.keys():
+  print(k)
+
+for v in dic1.values():
+  print(v)
+
