d628e56b710938ea5ca8929d7cb05b440e86e545
OtakeYuto <yuto.programming@gmail.com>
2023-01-11 17:28:33 +0900

    対比

---
+++ functions2,py
@@ -0,0 +1,5 @@
+def multi5(n):
+  return n * 5
+
+def _division5(n):
+  return n / 5
--- module.py
+++ module.py
@@ -1,7 +1,9 @@
 import math
 import functions
+from functions2 import *
 
 number = 9
 print(math.sqrt(number))
 
+print(dir(__name__))
 print(functions.minus5(number))
