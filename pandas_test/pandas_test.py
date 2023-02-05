379345942220425adb0f27fe36d9ce977680badd
OtakeYuto <yuto.programming@gmail.com>
2023-02-05 22:11:37 +0900

    DataFrame作成

--- pandas_test/pandas_test.py
+++ pandas_test/pandas_test.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
+df1 = pd.DataFrame({
+  'Name': ['Taro', 'Ichiro', 'Jiro'],
+  'Add': ['Tokyo', 'Osaka', 'Tokyo'],
+  'Age': [20, 21, 20]
+})
+print(df1)
+
 df = pd.read_excel('pandas_test/data.xlsx')
 print(df)
