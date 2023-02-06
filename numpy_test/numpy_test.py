e46fc2b5fcd07aa0fd5571ee9cb3bf4151889bdb
OtakeYuto <yuto.programming@gmail.com>
2023-02-07 01:06:52 +0900

    numpy 四則演算

---
+++ numpy_test/numpy_test.py
@@ -0,0 +1,19 @@
+import numpy as np
+x = np.array([10, 20, 30])
+print(x)
+
+y = np.array([100, 200, 300])
+print(y)
+
+# 足し算
+print(x + y)
+
+# 引き算
+print(x - y)
+
+# 掛け算
+print(x * y)
+
+# 割り算
+print(x / y)
+
